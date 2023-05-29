11a33b46b0498eb378875d9c45a48a6156ce5c1e
Dustin Ober <dustinober@Dustins-MacBook-Pro.local>
2023-05-28 20:30:11 -0400

    New Course Files 05-28-23

---
+++ Udemy C Programming for Beginners/EnterValue/EnterValue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+    char str[100];
+    int i;
+
+    printf("Enter a Value ");
+
+    scanf("%d", &i);
+
+    printf("\nYou entered: %d\n", i);
+
+    return 0;
+}
